d20432a394717c71da345f4d604055f22fcd28f3
guye1296 <ezer.guy@gmail.com>
2021-09-28 00:31:55 +0300

    seperate components from contents

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,24 +1,9 @@
 import * as React from "react"
-
-class ChallengePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div classname="challenge-page">
-        <h1>Challenge #{this.props.index}: {this.props.name}</h1>
-        <p>Challenge contents goes here</p>
-        <div><p>Linux terminal goes here</p></div>
-      </div>
-    );
-  }
-}
+import ChallengeLayout from "../components/challenge_layout"
 
 const IndexPage = () => {
   return(
-    <ChallengePage
+    <ChallengeLayout
       index="0"
       name="hello"
     />
